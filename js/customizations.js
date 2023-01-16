819a947ad8ff916ca8e19e82726359e20f596824
Sia Simeonova <s.simeonova@tinqin.com>
2023-01-16 16:34:07 +0200

    Fix for normal account.

--- js/customizations.js
+++ js/customizations.js
@@ -5,7 +5,7 @@ $(function () {
         let fields = document.getElementsByClassName('justify-content-between');
         for (let field of fields) {
             let title = field.getAttribute('title');
-            if(title !== 'Computers' && title !== 'Contracts' && title !== 'Tickets'){
+            if(title !== 'Computer' &&  title !== 'Computers' && title !== 'Contracts' && title !== 'Tickets'){
                 field.classList.add("d-none");
             }
         }
