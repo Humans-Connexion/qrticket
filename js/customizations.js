a84b2bff2d96bf3a77ea4524c2974b51a5101539
Sia Simeonova <s.simeonova@tinqin.com>
2023-01-16 11:32:06 +0200

    Fix bug - hidung of fiels only for computers page.

--- js/customizations.js
+++ js/customizations.js
@@ -1,10 +1,13 @@
 $(function () {
 
-    let fields = document.getElementsByClassName('justify-content-between');
-    for (let field of fields) {
-        let title = field.getAttribute('title');
-        if(title !== 'Computers' && title !== 'Contracts' && title !== 'Tickets'){
-            field.classList.add("d-none");
+    let here = document.getElementsByClassName('here')[0];
+    if(here != undefined && here.getAttribute('title') == 'Computers' ){
+        let fields = document.getElementsByClassName('justify-content-between');
+        for (let field of fields) {
+            let title = field.getAttribute('title');
+            if(title !== 'Computers' && title !== 'Contracts' && title !== 'Tickets'){
+                field.classList.add("d-none");
+            }
         }
     }
 });
